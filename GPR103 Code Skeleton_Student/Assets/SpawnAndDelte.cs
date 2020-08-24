27a530856c0ee083a6375dd9ff72fca9c5d2df4f
AlexMackMoo <alexander.macklin@mds.torrens.edu.au>
2020-08-24 11:42:21 +1000

    Infinite Loop trying to spawn items
    Well that was fun. I crashed, woke up, and got stuck in an infinte loop cause I change > to <. Fun

---
+++ GPR103 Code Skeleton_Student/Assets/SpawnAndDelte.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAndDelte : MonoBehaviour
+{
+    [Header ("Setting the object")]
+    public int amountOfEnemy;
+    public GameObject Car1;
+
+    [Header("Managing time")]
+    public const float TTL = 5;
+    private float timeCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amountOfEnemy = 0;
+        timeCount = TTL;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCount -= Time.deltaTime * 3f;
+        if (timeCount <= 0)
+        {
+            do
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    Instantiate(Car1);
+                    amountOfEnemy += 1;
+                }
+            } while (amountOfEnemy <= 1);
+        }
+
+        if (transform.position.x == -10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
